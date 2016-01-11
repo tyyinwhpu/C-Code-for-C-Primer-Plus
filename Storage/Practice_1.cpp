7068894b8d09a716ecb36e32f9075c984c9fcc0a
tyyyinwhpu <2668791974@qq.com>
2016-01-11 19:48:00 +0800

    my fourth commit for Branchch_1

--- Storage/Practice_1.cpp
+++
@@ -1,9 +0,0 @@
-//this program is to show your name and adress
-# include <iostream>
- int main ()
- {
-	 using namespace std;
-	 cout <<"hello,world."<<endl<<"tyyinwhpu in Wuhan` China`HuBei Province";
-	 return 0;
- }
- 
---
+++ Storage_2/Practice_1.cpp
@@ -0,0 +1,9 @@
+//this program is to show your name and adress
+# include <iostream>
+ int main ()
+ {
+	 using namespace std;
+	 cout <<"hello,world."<<endl<<"tyyinwhpu in Wuhan` China`HuBei Province";
+	 return 0;
+ }
+ 
